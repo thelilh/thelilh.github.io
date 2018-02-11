2925f1adbf7526e8a95337e6ab1336630470240f
Leighton Lilford <lrlilford@gmail.com>
2018-02-11 21:48:31 +0800

    Minor updates - Make gif hidden before page loads (looks better on slow connections) - Add 'cursor' to 'Yeah' - Enclose 'Yeah' in span

--- hug/script.js
+++ hug/script.js
@@ -2,6 +2,3 @@ var huggif = document.getElementById("huggif");
 document.getElementById("yeshug").addEventListener("click", function (event) {
     huggif.style.visibility = "visible";
 }, false);
-document.body.onload = function () {
-    huggif.style.visibility = "hidden";
-};
