473229b3a528f560fa2e1d2e3bda4f95001c2acf
Queerism <gamemod.mod@gmail.com>
2018-02-21 14:21:57 +0100

    Fixed H.U.G

--- hug/script.js
+++ hug/script.js
@@ -1,7 +1,15 @@
-var huggif = document.getElementById("huggif");
+var huggif, y, yeshug;
+huggif = document.getElementById("huggif");
+yeshug = document.getElementById("yeshug");
+y = 0;
 document.getElementById("yeshug").addEventListener("click", function (event) {
-    huggif.style.visibility = "visible";
+    if (y < 1) {
+        y = 1;
+        yeshug.innerHTML = "Thank you";
+        huggif.style.visibility = "visible";   
+    } else {
+        y = 0;
+        yeshug.innerHTML = "Yeah...";
+        huggif.style.visibility = "hidden";   
+    }
 }, false);
-document.body.onload = function () {
-    huggif.style.visibility = "hidden";
-};
