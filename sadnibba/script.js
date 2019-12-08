01d9bb8d8b9b33c2d405bd52995d4f651fb27f70
the_lil_h <gamemod.mod@gmail.com>
2019-12-08 22:10:19 +0100

    Yes

--- sadnibba/script.js
+++
@@ -1,7 +0,0 @@
-var sadnibbatxt = document.getElementById("sadnibbatxt");
-document.getElementById("sadnibba").addEventListener("click", function (event) {
-    sadnibbatxt.style.visibility = "visible";
-}, false);
-document.body.onload = function () {
-    sadnibbatxt.style.visibility = "hidden";
-};
