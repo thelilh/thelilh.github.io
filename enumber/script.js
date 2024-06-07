18a57a28eacb42d8576f02dfe545b86b857c3878
hannahkarlsson <hannah.karlsson@novacura.com>
2024-06-07 21:43:51 +0200

    Simple E-Number Search

--- enumber/script.js
+++ enumber/script.js
@@ -1,39 +1,84 @@
 class ENumber {
-    constructor(name, num, status) {
+    constructor(name = "", num = "", status = "") {
         this.Name = name
         this.Number = num;
         this.VeganStatus = status;
     }
 }
 
-function SearchForENumber(query) {
-    var list = GetAllENumbers();
-
-    var result = list.filter(function(x) {
-        if (query.length <= 0) {
-            return true;
-        }
-        if (x.Name.toLowerCase() == query.toLowerCase()
-            || x.Name.toLowerCase().includes(query.toLowerCase())
-            || x.Name.toLowerCase().startsWith(query.toLowerCase()
-            || x.Number.toLowerCase().includes(query.toLowerCase())
-            || x.Number.toLowerCase().startsWith(query.toLowerCase())
-            || x.Number.toLowerCase() == query.toLowerCase())
-            || x.VeganStatus.toLowerCase().includes(query.toLowerCase())
-            || x.VeganStatus.toLowerCase().startsWith(query.toLowerCase())
-            || x.VeganStatus.toLowerCase() == query.toLowerCase()) {
-                return true;
-            }
-
-        return false;
-    
-    })
-
-    return result;
-}
+let eNumberTable = document.getElementById("eNumberTable");
 
 function GetAllENumbers() {
-    return [new ENumber("Test", "100", "yes")];
+    return [
+        new ENumber("Curcumin", "100", "vegan"),
+        new ENumber("Riboflavin", "101", "vegan"),
+        new ENumber("Riboflavin-5'-Phosphate", "101a", "vegan"),
+        new ENumber("Tartrazine", "102", "vegan"),
+        new ENumber("Alkannin", "103", "Vegan"),
+        new ENumber("Quinoline Yellow WS", "104", "Vegan"),
+        new ENumber("Fast Yellow AB", "105", "vegan"),
+        new ENumber("Riboflavin-5-Sodium Phosphate", "106", "vegan"),
+        new ENumber("Yellow 2G", "107", "vegan"),
+        new ENumber("Sunset Yellow FCF", "110", "vegan"),
+        new ENumber("Orange GGN", "111", "vegan"),
+        new ENumber("Cochineal, Carminic acid, Carmine", "120", "no"),
+        new ENumber("Citrus Red 2", "121", "vegan")
+        // TODO: Add more E-Numbers
+    ];
 }
 
-console.log()
+function GenerateTable() {
+    GetAllENumbers().forEach((e) => {
+        var style = "";
+        if (e.VeganStatus.toLowerCase() == "vegan") {
+            style = "background-color: green !important; color: white;"
+        }
+        else if (e.VeganStatus.toLowerCase().includes("vegetarian")) {
+            style = "background-color: yellow !important; color: black;"
+        }
+        else {
+            style = "background-color: red !important; color: white;"
+        }
+        eNumberTable.innerHTML += "<tr><td>" + e.Name + "</td><td>" + e.Number + "</td><td style=\"" + style + "\">" + Capitalize(e.VeganStatus) + "</td></tr>"
+    })
+}
+GenerateTable()
+
+function Capitalize(text = "") {
+    var split = text.split(/-| /);
+    if (split.length <= 1)
+        return text.charAt(0).toUpperCase() + text.substring(1)
+
+    var test = ""
+    for (let i = 0; i < split.length; i++) {
+        var splitAtI = split[i];
+        test += splitAtI.charAt(0).toUpperCase() + splitAtI.substring(1) + " "
+    }
+
+    return test;
+}
+
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("eNumberTable");
+    header = document.getElementById("header");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                if (i != 0) {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+}
