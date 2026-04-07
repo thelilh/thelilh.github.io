455e0cdda6067696090e4f1891be268bfabb567a
lovisen <theleafinwind@proton.me>
2026-04-07 19:36:51 +0200

    Fixed map being silly

--- hideandseek/map/index.js
+++ hideandseek/map/index.js
@@ -1,4 +1,30 @@
-document.getElementById('map').addEventListener('load', function(){
-  // Will get called after embed element was loaded
-  svgPanZoom(document.getElementById('map'));
+document.getElementById('map').addEventListener('load', function () {
+    // Will get called after embed element was loaded
+    svgPanZoom();
+    var options = {
+        zoomEnabled: true
+        , controlIconsEnabled: true
+        , customEventsHandler: {
+            // Halt all touch events
+            haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+
+            // Init custom events handler
+            , init: function (options) {
+                // Init Hammer
+                this.hammer = Hammer(options.svgElement)
+
+                // Handle double tap
+                this.hammer.on('doubletap', function (ev) {
+                    options.instance.zoomIn()
+                })
+            }
+
+            // Destroy custom events handler
+            , destroy: function () {
+                this.hammer.destroy()
+            }
+        }
+    }
+
+    svgPanZoom(document.getElementById('map'), options);
 })
